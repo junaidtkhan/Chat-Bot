fffe02658873446980da27e918b55734ee6fd978
junaidtkhan <89198741+junaidtkhan@users.noreply.github.com>
2023-03-31 16:09:08 +0500

    firestore successfully added and working fine

---
+++ vite-project/src/fetchChatFromFIreStore.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { arrayUnion, collection, getDoc, getDocs, query, updateDoc, where } from 'firebase/firestore'
+import { db } from './main'
+import { getAuth } from 'firebase/auth'
+
+export const fetchChatFromFIreStore = () => {
+    const auth = getAuth()
+
+    const q = query(collection(db, "users"), where("uid", "==", auth.currentUser.uid))
+
+    getDocs(q).then((res) => {
+        res.forEach((document) => {
+            const docRef = doc(db, 'users', document.id)
+            getDoc(docRef).then((res) => {
+                const chat = res.data().chat
+                return chat
+            }).catch((err) => { err.message })
+        })
+    }).catch((err) => { console.log(err.message) })
+}
